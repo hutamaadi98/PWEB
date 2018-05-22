ec5e445df655d5903f7872aeb9829cc3a135f300
hutamaadi98 <33992171+hutamaadi98@users.noreply.github.com>
2018-05-22 19:39:11 +0700

    Add files via upload

--- tambahMobil.php
+++ tambahMobil.php
@@ -1,30 +1,20 @@
 <?php
-
 	$mysqli = new mysqli('localhost','root','','pweb');
 	if ($mysqli->connect_errno)
 	{
 		echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 	}
-
-	$sql = "SELECT * FROM merk";
+	$sql = "SELECT idmobil from mobil order by idmobil Desc limit 1";
 	$stmt = $mysqli->query($sql);
-
-	$mysqli -> close();
 ?>
 
-<form>
+<form method = "POST" enctype="multipart/form-data" id="formupload">
 	<h1> Input Mobil</h1>
 	<p> Tipe : <input type="text" name="tipe" id="tipe"></p>
 	<p>Merk: 
-		<select name="tipe" id="merk">
+		<select name="merk" id="merk">
 			<option value="">Pilih Merk</option></select></p>
-			<!--<?php
-				//while($row = $stmt -> fetch_assoc())
-				//{
-				    //echo "<option value='".$row['idmerk']."'>".$row['nama']."</option>";
-				//}
-			?>
-		</select>-->
+		</select>
 	<p>Panjang : <input type="text" name="panjang" id="panjang"></p>
 	<p>Lebar : <input type="text" name="lebar" id="lebar"></p>
 	<p>Tinggi : <input type="text" name="tinggi" id="tinggi"></p>
@@ -36,36 +26,18 @@
 	<p>Kapasitas Tangki : <input type="text" name="kapasitastangki" id="kapasitastangki"></p>
 	<p>Ukuran Velg : <input type="text" name="ukuranvelg" id="ukuranvelg"></p>
 	<p>Ukuran Roda : <input type="text" name="ukuranroda" id="ukuranroda"></p>
+	<p>Foto : <input type="file" name="foto" size="50" accept=".jpg" id="foto"></p><BR>
 	<p><input type="button" value="Simpan" id="submit"></p>
+	<?php
+		while($row = $stmt -> fetch_assoc())
+		{
+			echo "<input type='hidden' name='idmobil' id='idmobil' value='". ($row['idmobil']+1) ."'>";	  
+		}
+	?> -->
 </form>
 
+	<div id="gambar"></div>
 <script src="jquery/jquery-2.1.4.min.js"></script>
-
-<script>
-	$("#submit").click(function() {
-		$.post("tambahMobil_ajax.php",
-		{
-			tipe: $("#tipe").val(),
-			merk: $("#merk").val(),
-			panjang: $("#panjang").val(),
-			lebar: $("#lebar").val(),
-			tinggi: $("#tinggi").val(),
-			jaraksumburoda: $("#jaraksumburoda").val(),
-			radiusputar: $("#radiusputar").val(),
-			hargaminimal: $("#hargaminimal").val(),
-			hargamaximal: $("#hargamaximal").val(),
-			kapasitasmesin: $("#kapasitasmesin").val(),
-			kapasitastangki: $("#kapasitastangki").val(),
-			ukuranvelg: $("#ukuranvelg").val(),
-			ukuranroda: $("#ukuranroda").val()
-		})
-
-		.done(function(data) {
-			alert(data);
-		})
-	})
-</script>
-
 <script>
 	$("#merk").focusin(function() {
 		$.post("comboBox.php" , {
@@ -76,4 +48,28 @@
 			$("#merk").html(data);
 		})
 	});
+</script>
+<script>
+	$("#submit").click(function(){
+		var formData = new FormData($("#formupload")[0]);
+		$.ajax({
+			url:'tambahMobil_ajax.php',
+			type:'POST',
+			data: formData,
+       		async: false,
+       		cache: false,
+       		contentType: false,
+       		enctype: 'multipart/form-data',
+       		processData: false,
+       		success: function (response) {
+       			if (response == 'Failed')
+         			alert(response);
+         		else
+         		{
+         			alert(response);
+         			$("#gambar").html(response);
+       			}
+		}
+	})
+	})
 </script>
--- tambahMobil_ajax.php
+++ tambahMobil_ajax.php
@@ -1,5 +1,4 @@
 <?php
-
 	$mysqli = new mysqli("localhost","root","","pweb");
 
 	$tipe = $_POST['tipe'];
@@ -15,7 +14,20 @@
 	$kapasitastangki = $_POST['kapasitastangki'];
 	$ukuranvelg = $_POST['ukuranvelg'];
 	$ukuranroda = $_POST['ukuranroda'];
+	$idmobil= $_POST['idmobil'];
 
+	echo $idmobil;
+
+	if ($_FILES['foto']['type'] == "image/jpeg")
+        {
+            $dest = "gambar/".$idmobil.substr($_FILES["foto"]["name"],-4);
+            $hasil = move_uploaded_file($_FILES["foto"]["tmp_name"], $dest);
+            echo "<img src=".$dest."></img>";
+        }
+        else
+            {
+            	echo "Failed";
+    }
 	$sql = "insert into mobil(idmerk,tipe,panjang,lebar,tinggi,jarak_sumbu_roda,radius_putar,harga_min,harga_max,kapasitas_mesin,kapasitas_tangki,ukuran_velg,ukuran_roda) values('$merk','$tipe','$panjang','$lebar','$tinggi','$jaraksumburoda','$radiusputar','$hargaminimal','$hargamaximal','$kapasitasmesin','$kapasitastangki','$ukuranvelg','$ukuranroda')";
 	$hasil = $mysqli->query($sql);
 	
