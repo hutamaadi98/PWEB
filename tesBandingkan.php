16e539e10ee7634aba0ae72964632beea6e8ef61
senryakuka <38880272+senryakuka@users.noreply.github.com>
2018-05-23 21:15:07 +0700

    Add files via upload

--- tesBandingkan.php
+++ tesBandingkan.php
@@ -57,42 +57,171 @@ $total = count($name);
 		echo "
 		<table border = '1'>	
 			<tr> 
-				<th>Gambar</th>
-				<th>Merk</th> 
-				<th>Tipe</th> 
-				<th>Panjang</th> 
-				<th>Lebar</th> 
-				<th>Tinggi</th> 
-				<th>Jarak sumbu roda</th> 
-				<th>Radius Putar</th> 
-				<th>Harga min</th> 
-				<th>Harga max</th> 
-				<th>Kapasitas mesin</th>
-				<th>Kapasitas tangki</th>
-				<th>Ukuran velg</th>
-				<th>Ukuran roda</th>
+				<th>Gambar</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td><img width='200' height='150' src='gambar/".$mobilbanding[$i]['idmobil'].".jpg'></td>";
+				}
+				
+			echo "
+			</tr>	
+			<tr>
+				<th>Merk</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['nama'] . "</td>";
+				}
+			
+			echo "
+			</tr>
+			<tr>
+				<th>Tipe</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['tipe'] . "</td>";
+				}
+			echo "
+			</tr>
+			<tr>
+				<th>Panjang</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['panjang'] . "</td>";
+				}
+
+			echo" 
+			</tr>
+			<tr>
+				<th>Lebar</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['lebar'] . "</td>";
+				}
+
+			echo" 
+			</tr>
+			<tr>
+				<th>Tinggi</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['tinggi'] . "</td>";
+				}
+
+			echo"
+			</tr>
+			<tr>
+				<th>Jarak sumbu roda</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['jarak_sumbu_roda'] . "</td>";
+				}
+
+			echo"
+			</tr>
+			<tr>
+				<th>Radius Putar</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['radius_putar'] . "</td>";
+				}
+
+			echo"
+			</tr>
+			<tr>
+				<th>Harga Minimal</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['harga_min'] . "</td>";
+				}
+			echo"
+			</tr>
+			<tr>
+				<th>Harga Maximal</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['harga_max'] . "</td>";
+				}
+			echo"
+			</tr>
+			<tr>
+				<th>Kapasitas Mesin</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['kapasitas_mesin'] . "</td>";
+				}
+			echo"
+			</tr>
+			<tr>
+				<th>Kapasitas Tangki</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['kapasitas_tangki'] . "</td>";
+				}
+			echo"
+			<tr>
+				<th>Ukuran Velg</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['ukuran_velg'] . "</td>";
+				}
+			echo"
+			</tr>
+			<tr>
+				<th>Ukuran Roda</th>";
+				for($i = 0; $i <= ($total-1) ; $i++)
+				{
+					echo "<td>". $mobilbanding[$i]['ukuran_roda'] . "</td>";
+				}
+			echo"
+			</tr>
 			</tr>
 		";
 
-	    foreach ($mobilbanding as $row) 
+	    /*foreach ($mobilbanding as $row) 
 	    { 
 	        echo '<tr>';
 	        echo '<td><img width="200" height="150" src="gambar/'.$row['idmobil'].'.jpg"></td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['nama'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['tipe'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['panjang'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['lebar'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['tinggi'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['jarak_sumbu_roda'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['radius_putar'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['harga_min'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['harga_max'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['kapasitas_mesin'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['kapasitas_tangki'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['ukuran_velg'] . '</td>';
+	        echo '</tr>';
+	        echo '<tr>';
 	        echo '<td>' . $row['ukuran_roda'] . '</td>';
 	        echo '</tr>';
-    	}
+    	}*/
 
     	echo"</table>";
 		/*echo "
@@ -121,7 +250,7 @@ $total = count($name);
 		echo"</table> ";*/
 		$mysqli->close();
 
-		//print_r($mobilbanding);
+		/*print_r($mobilbanding);*/
 		echo "<BR><BR>";
 		echo "
 		<a href='listMobilBandingkan.php'>Kembali</a>";
